7eaabd41bb1df7d762155a60c4bfab427d18fbfc
edunoodt <edunoodt@yahoo.com.ar>
2023-05-25 01:26:44 -0300

    primeras lineas del sistema Clinica

---
+++ Clinica/soporte.py
@@ -0,0 +1,74 @@
+from os import system
+import platform
+
+def borra():
+    siso = platform.system()
+    match siso: 
+        case 'Windows':
+            system ('cls')
+        case 'Linux':
+            system('clear')
+
+def inicializaPac():
+    dicti = {}
+    try:
+        with open('Clinica/info/pacientes.txt','r') as archi:
+            salida = archi.readline()
+            while salida != '':
+                claveb = True
+                clave = ''
+                lista = []
+                palabra = ''
+                for letra in salida:
+                    if letra != ';' and letra != '\n':
+                        palabra = palabra + letra
+                    elif claveb:
+                        clave = palabra
+                        claveb = False
+                        palabra =''
+                    else:
+                        lista.append(palabra)
+                        palabra = ''
+                dicti[clave]=lista
+                salida = archi.readline()
+    except FileNotFoundError:
+        archi = open ('Clinica/info/pacientes.txt','w')
+        archi.close()
+    return dicti
+
+def ingresoDNI():
+    print ('\n=============================================================================================')
+    print ('El documento debe contener 8 caracteres.  Agregar 0 para documentos con menos digitos\n')
+    print (' Para salir del sistema presione "***" ')
+    cont = 0
+    dni=''
+    while len(dni) != 8 and cont < 3 and dni != '***':
+        print ('Intento {}'.format(cont+1))
+        dni = input ('Ingrese DNI del Paciente ===>>>>  ')
+        cont += 1
+    if cont == 3:
+        print ('*******************************\nVerifique número de documento\n')
+        return 'error'
+    elif dni == '***':
+        return '***'
+    else:
+        return dni
+
+def verificaDNI(dni,dicti):
+    return dni in dicti.keys()
+
+def altaDNI(dni,pac):    
+    nombre = input ('Nombre del paciente: ')
+    apellido = input ('Apellido del paciente: ')
+    nac = input ('fecha de nacimiento (ddmmaaaa): ')
+    
+    linea = (dni+ ';' + nombre + ';' + apellido + ';' + nac + '\n')
+    with open('Clinica/info/pacientes.txt','a') as archi:
+        archi.write(linea)
+
+    lista = [nombre,apellido,nac]
+    pac[dni]= lista
+    return pac
+
+
+
--- ejemplos.py
+++ ejemplos.py
@@ -1,4 +1,4 @@
-coins = ["Bitcoin", "Ethereum", "Cardano"]
+"""coins = ["Bitcoin", "Ethereum", "Cardano"]
 prices = [48000,2585,2]
 for i, coin in enumerate(coins):
     price = prices[i]
@@ -8,9 +8,9 @@ print('***********************************\n\n')
 dict1 = {1: "Bitcoin", 2: "Ethereum"}
 for key, value in dict1.items():
     print(f"Key {key} has value {value}")
+"""
 
-
-dict2 = {12472451:['Jorge','Gomez'], 36784512:['Daniel','Gutierrez']}
+dict2 = {12472451:['Jorge','Gomez'], 36784512:['Daniel','Gutierrez'],25896541:['Juan','Perez']}
 texto2=''
 for clave,contenido in dict2.items():
      texto1 = (str(clave)+ ';' + contenido[0] + ';' + contenido[1]+'\n')
@@ -18,13 +18,30 @@ for clave,contenido in dict2.items():
 
 texto = 'los leones del africa son vegetarianos\nPero de cualquier modo, no me acercaria demasiado\n'
 
-with open('archivo.txt','w') as archi:
+with open('prueba/archivo.txt','w') as archi:
         
         archi.write(texto2)
         #archi.write(texto)
 
 print ('*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*')
-with open('archivo.txt','r') as archi:
-     print(archi.readlines())
-
-
+dicti = {}
+with open('prueba/archivo.txt','r') as archi:
+     salida = archi.readline()
+     while salida != '':
+        claveb = True
+        clave = ''
+        lista = []
+        palabra = ''
+        for letra in salida:
+            if letra != ';' and letra != '\n':
+                palabra = palabra + letra
+            elif claveb:
+                clave = palabra
+                claveb = False
+                palabra =''
+            else:
+                 lista.append(palabra)
+                 palabra = ''
+        dicti[clave]=lista
+        salida = archi.readline()
+print (dicti)
