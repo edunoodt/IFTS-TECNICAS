3c5fdd50639932647571b1d1cddb2b01edeca238
edunoodt <edunoodt@yahoo.com.ar>
2023-06-01 18:02:00 -0300

    se retiran lineas inutiles

--- prueba.py
+++ prueba.py
@@ -11,11 +11,3 @@ def ter():
 lista = [pri, seg, ter]
 opc = int(input ('====>>>> '))-1
 lista[opc]()
-
-
-#[pri, seg, ter][opc]()
-
-
-#cuenta = lambda x,y,z : (x *(1+ y))**(1/z)
-#print (cuenta (6.2,0.3,3))
-
