d849a9fc96db9ab0dd51dccdf0a34310b4b807c6
edunoodt <edunoodt@yahoo.com.ar>
2023-06-02 09:02:14 -0300

    Pruebas con la ruta de acceso y literalidad del back slash

--- Establecimientos/prueba.py
+++ Establecimientos/prueba.py
@@ -1,6 +1,10 @@
 from os import system
 system ('cls')
 
-with open ('Establecimientos_salud.txt','r') as arca:
+with open (r'C:\Users\eduno\OneDrive\Documents\IFTS-TECNICAS\Establecimientos\Establecimientos_salud.txt','r') as arca:
     lista = arca.read()
     print (lista)
+
+
+    #C:\Users\eduno\OneDrive\Documents\IFTS-TECNICAS\Establecimientos\Establecimientos_salud.txt
+    #Establecimientos\Establecimientos_salud.txt
